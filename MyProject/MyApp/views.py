66d3e03e37539ba0e3942cb905077ed791c49192
ADMIN <rainbowkun2@gmail.com>
2024-02-25 17:51:37 +0300

    Hello World!!

--- MyProject/MyApp/views.py
+++ MyProject/MyApp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def helloworld(request):
     return render(request, 'helloworld.html')
+def main_page(request):
+    return render(request, 'main_page.html')
+def comment(request):
+    return render(request, 'comment.html')
