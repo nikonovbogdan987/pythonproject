fa367f214b7fb0fcdf21f875a4819f816599c358
ADMIN <rainbowkun2@gmail.com>
2024-02-25 18:07:06 +0300

    Hello World!!

--- MyProject/MyApp/urls.py
+++ MyProject/MyApp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.helloworld, name='helloworld'),
-    path('main_page', views.main_page, name='main_page'),
-    path('comment', views.comment, name='commment'),
+    path('main_page/', views.main_page, name='main_page'),
+    path('comment/', views.comment, name='commment'),
 ]
