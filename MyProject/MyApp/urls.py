b9f9a649798f4be8961a8c6ac0d8eb919667540e
ADMIN <rainbowkun2@gmail.com>
2024-02-26 18:12:49 +0300

    BEGGINIG OF MY PROJECT!

--- MyProject/MyApp/urls.py
+++ MyProject/MyApp/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.helloworld, name='helloworld'),
-    path('main_page/', views.main_page, name='main_page'),
-    path('comment/', views.comment, name='commment'),
+    path('', views.main, name='main'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('for_student/', views.for_student, name='for_student'),
+    path('programs_and_courses/', views.programs_and_courses, name='programs_and_courses'),
+    path('python/', views.python, name='python'),
+    path('C_plus/', views.C_plus, name='C_plus'),
+    path('web/', views.web, name='web'),
+    path('support/', views.support, name='support'),
 ]
--- MyProject/MyApp/views.py
+++ MyProject/MyApp/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-def helloworld(request):
-    return render(request, 'helloworld.html')
-def main_page(request):
-    return render(request, 'main_page.html')
-def comment(request):
-    return render(request, 'comment.html')
+def main(request):
+    return render(request, 'main.html')
+def for_student(request):
+    return render(request, 'for_student.html')
+def about_us(request):
+    return render(request, 'about_us.html')
+def programs_and_courses(request):
+    return render(request, 'programs_and_courses.html')
+def support(request):
+    return render(request, 'support.html')
+def python(request):
+    return(request, 'python.html')
+def C_plus(request):
+    return(request, 'C_plus.html')
+def web(request):
+    return(request, 'web.html')
+
