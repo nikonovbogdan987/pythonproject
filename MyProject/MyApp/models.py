f9dd18ff4eea7b289c8d2d2e8baf0e34b6e7b9ee
ADMIN <rainbowkun2@gmail.com>
2024-04-06 14:41:49 +0300

    Popitka create bd

--- MyProject/MyApp/models.py
+++ MyProject/MyApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Clients(models.Model):
+    name = models.CharField("ФИО", max_length=50)
+    login = models.CharField("Логин",max_length=50)
+    email = models.EmailField("Эл.почта", max_length=50)
+    phone = models.CharField("Телефон", max_length=11)
+    password = models.CharField("Пароль", max_length=20)
+
+    def __str__(self) -> str:
+        return self.name
+
+        # переименование в админ-панели название таблицы
+    class Meta:
+        verbose_name = "клиента"
+        verbose_name_plural = "Клиенты"
