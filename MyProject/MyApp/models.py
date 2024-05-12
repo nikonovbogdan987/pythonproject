d551becd2d47eb3d375dcb5e4388a4c1b54cc6b4
ADMIN <rainbowkun2@gmail.com>
2024-05-12 17:30:05 +0300

    new models

--- MyProject/MyApp/models.py
+++ MyProject/MyApp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Clients(models.Model):
-    name = models.CharField("ФИО", max_length=50)
-    login = models.CharField("Логин",max_length=50)
+    name = models.CharField("Имя", max_length=50)
+    surname = models.CharField("Фамилия",max_length=50)
     email = models.EmailField("Эл.почта", max_length=50)
     phone = models.CharField("Телефон", max_length=11)
     password = models.CharField("Пароль", max_length=20)
@@ -11,7 +11,18 @@ class Clients(models.Model):
     def __str__(self) -> str:
         return self.name
 
-        # переименование в админ-панели название таблицы
+    # переименование в админ-панели название таблицы
     class Meta:
         verbose_name = "клиента"
         verbose_name_plural = "Клиенты"
+class Orders(models.Model):
+    author = models.ForeignKey(Clients, on_delete=models.CASCADE)
+    data = models.DateField("Дата заказа")
+    name_game = models.CharField("Название игры", max_length=50)
+    price = models.IntegerField("Цена игры")
+    def __str__(self) -> str:
+        return self.name
+    class Meta:
+        verbose_name = "заказ"
+        verbose_name_plural = "Заказы"
+
