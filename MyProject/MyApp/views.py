8dbd66d199d49cf86badb3de29db32f12702e313
ADMIN <rainbowkun2@gmail.com>
2024-05-24 20:01:49 +0300

    registration back

--- MyProject/MyApp/views.py
+++ MyProject/MyApp/views.py
@@ -10,6 +10,26 @@ def support(request):
 def Console(request):
     return render(request, 'Console.html')
 def page_of_registration(request):
+    def registration(request):
+        name = ''
+        surname = ''
+        email = ''
+        phonenumber = ''
+        password = ''
+        if request.method == 'POST' and 'login_btn' in request.POST:
+            name = request.POST['username']
+            surname = request.POST['usersurname']
+            email = request.POST['useremail']
+            phonenumber = request.POST['userphonenumber']
+            password = request.POST['userpassword']
+        context = {
+            'name' : name,
+            'surname' : surname,
+            'email' : email,
+            'phonenumber' : phonenumber,
+            'password' : password
+        }
+        return render(request, 'banana.html', context)
     return render(request, 'page_of_registration.html')
 
 
